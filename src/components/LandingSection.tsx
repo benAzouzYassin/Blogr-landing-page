d0e14ee636b3e5fd038ae040104432f62680de72
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-05-06 21:48:58 +0200

    finished the landing section

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -2,11 +2,17 @@ import Navbar from "./Navbar"
 
 function LandingSection() {
     return (
-        <section className=" h-[100vh]  bg-gradient-to-r from-[#ff806d] to-[#ff4759]" >
+        <section className=" h-[100vh]  bg-gradient-to-r from-[#ff806d] to-[#ff4759] rounded-bl-[150px]" >
             <div className="h-full w-full lg:bg-[url('/images/bg-pattern-intro-desktop.svg')] bg-cover overflow-hidden bg-left">
                 <div className="pr-64 pl-64 ">
                     <Navbar />
-
+                    <main className=" flex flex-col h-[75vh] justify-center items-center">
+                        <h1 className="text-white text-6xl font-semibold ">A modern publishing platform</h1>
+                        <p className="mt-12 text-2xl font-light text-white">Grow your buisness and build your online brand</p>
+                        <div className="gap-5 flex mt-12 ">
+                            <button className=" hover:bg-gray-200 bg-white p-3 pl-5 pr-5 rounded-3xl text-[#fb7662] font-bold">Start for free</button>
+                            <button className="border-solid border-white border-2 p-3 pl-5 pr-5 rounded-3xl text-white font-bold hover:bg-[#ff806d]">Learn more </button></div>
+                    </main>
                 </div>
             </div>
         </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ function Navbar() {
         <div className="flex flex-row items-center pt-12 ">
             <span className="text-4xl font-bold text-white ">Blogr</span>
             <span className="flex gap-5 flex-row self-end ml-16 text-lg font-semibold text-white">
-                <a href="/product" className="flex items-center">Product<Arrow /></a>
-                <a href="/company" className="flex items-center">Company <Arrow /></a>
-                <a href="/connect" className="flex items-center">Connect <Arrow /></a>
+                <a href="/product" className="flex items-center hover:text-gray-200">Product<Arrow /></a>
+                <a href="/company" className="flex items-center hover:text-gray-200">Company <Arrow /></a>
+                <a href="/connect" className="flex items-center hover:text-gray-200">Connect <Arrow /></a>
             </span>
             <button className="text-white font-semibold hover:text-gray-300 ml-auto mr-5 p-3 pl-8 pr-8 rounded-3xl ">Login</button>
             <button className=" p-3 pl-8 pr-8 rounded-3xl bg-white  text-[#ff515b] font-semibold hover:bg-gray-200">Sign Up</button>
